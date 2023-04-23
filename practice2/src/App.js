10f74b7a648afcc047839ed41941b5f241709593
Lachlan Sim <lachlansim@gmail.com>
2023-04-23 18:26:23 +1200

    second commit

--- practice2/src/App.js
+++ practice2/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React New Feature text
         </a>
       </header>
     </div>
